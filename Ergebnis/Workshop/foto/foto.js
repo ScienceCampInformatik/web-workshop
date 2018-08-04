59b980012a877685f8c45783cf9f791fdc76eb3c
lenaWitterauf <lena.emma77@gmail.com>
2018-08-04 21:22:47 +0100

    Added more different Foto changing functions

--- Ergebnis/Workshop/foto/foto.js
+++ Ergebnis/Workshop/foto/foto.js
@@ -60,6 +60,41 @@ function schwarzWeiss() {
     updateFotoDownload();
 }
 
+function invertieren() {
+  var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  var data = imageData.data;
+
+    for(var i = 0; i < data.length; i += 4) {
+      // red
+      data[i] = 255-data[i];
+      // green
+      data[i + 1] = 255-data[i+1];
+      // blue
+      data[i + 2] = 255-data[i+2];
+    }
+
+    context.putImageData(imageData, 0, 0);
+    updateFotoDownload();
+}
+
+function rotBlau() {
+  var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  var data = imageData.data;
+
+    for(var i = 0; i < data.length; i += 4) {
+      var rot = data[i];
+
+      // red
+      data[i] = data[i+2];
+      // blue
+      data[i + 2] = rot;
+    }
+
+    context.putImageData(imageData, 0, 0);
+    updateFotoDownload();
+}
+
+
 
 
 function gestreift() {
